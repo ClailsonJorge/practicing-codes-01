673fae1ad8a50664a65da0ade1eaf0d518af6874
ClailsonJorge <clailsonjorge@gmail.com>
2020-08-27 08:43:33 -0300

    chore: add faker

--- src/domain/data/usecases/authentication/remote-authentication.spec.ts
+++ src/domain/data/usecases/authentication/remote-authentication.spec.ts
@@ -2,25 +2,24 @@ import { RemoteAuthentication } from './remote-authentication';
 import { HttpPostClient } from '../../protocols/http/http-post-client';
 import { HttpPostClientSpy } from '../../test/mock-http-client';
 
-
-type SutTypes{
+type SutTypes = {
   sut: RemoteAuthentication;
   httpPostClientSpy: HttpPostClientSpy;
-}
+};
 
-const makeSut = (url:string= 'any_url'):SutTypes =>{
+const makeSut = (url: string = 'any_url'): SutTypes => {
   const httpPostClientSpy = new HttpPostClientSpy();
   const sut = new RemoteAuthentication(url, httpPostClientSpy);
   return {
     sut,
-    httpPostClientSpy
-  }
-}
+    httpPostClientSpy,
+  };
+};
 
 describe('RemoteAuthentication', () => {
   test('Should call HttpPostClient with correct URL', async () => {
-    const url = 'other_url'
-    const {httpPostClientSpy, sut} = makeSut(url)
+    const url = 'other_url';
+    const { httpPostClientSpy, sut } = makeSut(url);
     await sut.auth();
     expect(httpPostClientSpy.url).toBe(url);
   });
