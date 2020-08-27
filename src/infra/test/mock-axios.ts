c37ec6eb11a0f8efe861c28fce1279fd40c63927
ClailsonJorge <clailsonjorge@gmail.com>
2020-08-27 20:14:11 -0300

    refactor: correct mock-axios

--- src/infra/test/mock-axios.ts
+++ src/infra/test/mock-axios.ts
@@ -3,7 +3,7 @@ import faker from 'faker';
 
 export const mockAxios = (): jest.Mocked<typeof axios> => {
   const mockedAxios = axios as jest.Mocked<typeof axios>;
-  mockedAxios.post.mockRejectedValue({
+  mockedAxios.post.mockResolvedValue({
     data: faker.random.objectElement(),
     status: faker.random.number(),
   });
