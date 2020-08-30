35d5a286e8f2dbfeae2396f6554d1bc9f2885d1f
ClailsonJorge <clailsonjorge@gmail.com>
2020-08-30 11:31:21 -0300

    feat: add login view

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
           {
             loader: 'css-loader',
             options: {
-              module: true,
+              modules: true,
             },
           },
           {
