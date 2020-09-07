0d1c1a052fbf7473996b3ccf2eb553205f6ad760
ClailsonJorge <clailsonjorge@gmail.com>
2020-09-07 13:23:23 -0300

    fix: copy login created cause bug in the jest

---
+++ src/presentation/pages/login/login.spec.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Login from '@/presentation/pages/login/loginTest';
+
+describe('Login Component', () => {
+  test('', () => {
+    render(<Login />);
+  });
+});
