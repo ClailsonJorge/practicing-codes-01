8b49e5a0a715b5b422f0da1f9adfe711901d9724
ClailsonJorge <clailsonjorge@gmail.com>
2020-09-07 15:20:23 -0300

    feat: ensure Login will not load spinner and error on start

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
+  roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '**/*.d.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
   transform: {
-    '.+\\.(ts |tsx)$': 'ts-jest',
+    '.+\\.tsx?$': 'ts-jest',
   },
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1',
