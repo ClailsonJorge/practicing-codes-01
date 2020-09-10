980b09b8bdb628e79bf9f1f2808d3e2406d3c5e0
ClailsonJorge <clailsonjorge@gmail.com>
2020-09-09 21:32:22 -0300

    test: ensure Login start with inputs with required state

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -7,7 +7,13 @@ describe('Login Component', () => {
     const { getByTestId } = render(<Login />);
     const errorWrap = getByTestId('error-wrap');
     expect(errorWrap.childElementCount).toBe(0);
-    const submit = getByTestId('submit') as HTMLButtonElement;
-    expect(submit.disabled).toBe(true);
+    const submitButton = getByTestId('submit') as HTMLButtonElement;
+    expect(submitButton.disabled).toBe(true);
+    const emailStatus = getByTestId('email-status');
+    expect(emailStatus.title).toBe('Campo obrigatório');
+    expect(emailStatus.textContent).toBe('🔴');
+    const passwordStatus = getByTestId('password-status');
+    expect(passwordStatus.title).toBe('Campo obrigatório');
+    expect(passwordStatus.textContent).toBe('🔴');
   });
 });
