d513ae091fb11286198ab3a7a761060eb10df1db
ClailsonJorge <clailsonjorge@gmail.com>
2020-09-11 19:36:59 -0300

    feat: ensure Login present error if password is invalid

--- src/presentation/test/mock-validation.ts
+++ src/presentation/test/mock-validation.ts
@@ -1,13 +1,9 @@
 import { Validation } from '@/presentation/protocols/validation';
 
-export class ValidationSpy implements Validation {
+export class ValidationStub implements Validation {
   erroMessage: string;
-  fildName: string;
-  fildValue: string;
 
   validate(fildName: string, fildValue: string): string {
-    this.fildName = fildName;
-    this.fildValue = fildValue;
     return this.erroMessage;
   }
 }
