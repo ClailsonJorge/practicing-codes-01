73aef481675e21196c1368a8dafa5cb6c08cfd21
ClailsonJorge <clailsonjorge@gmail.com>
2020-09-14 21:38:21 -0300

    refactor: change validationStub to initialize without error

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -14,10 +14,13 @@ type SutTypes = {
   validationStub: ValidationStub;
 };
 
-const makeSut = (): SutTypes => {
+type SutParams = {
+  validationError: string;
+};
+
+const makeSut = (params?: SutParams): SutTypes => {
   const validationStub = new ValidationStub();
-  const errorMessage = faker.random.words();
-  validationStub.erroMessage = errorMessage;
+  validationStub.erroMessage = params?.validationError;
   const sut = render(<Login validation={validationStub} />);
   return {
     sut,
@@ -27,43 +30,46 @@ const makeSut = (): SutTypes => {
 
 describe('Login Component', () => {
   afterEach(cleanup);
+
   test('Should not render spinner and error on start', () => {
-    const { sut, validationStub } = makeSut();
+    const validationError = faker.random.words();
+    const { sut } = makeSut({ validationError });
     const errorWrap = sut.getByTestId('error-wrap');
     expect(errorWrap.childElementCount).toBe(0);
     const submitButton = sut.getByTestId('submit') as HTMLButtonElement;
     expect(submitButton.disabled).toBe(true);
     const emailStatus = sut.getByTestId('email-status');
-    expect(emailStatus.title).toBe(validationStub.erroMessage);
+    expect(emailStatus.title).toBe(validationError);
     expect(emailStatus.textContent).toBe('🔴');
     const passwordStatus = sut.getByTestId('password-status');
-    expect(passwordStatus.title).toBe(validationStub.erroMessage);
+    expect(passwordStatus.title).toBe(validationError);
     expect(passwordStatus.textContent).toBe('🔴');
   });
 
   test('Should show email error if validation fails', () => {
-    const { sut, validationStub } = makeSut();
+    const validationError = faker.random.words();
+    const { sut } = makeSut({ validationError });
     const emailInput = sut.getByTestId('email');
     fireEvent.input(emailInput, { target: { value: faker.internet.email() } });
     const emailStatus = sut.getByTestId('email-status');
-    expect(emailStatus.title).toBe(validationStub.erroMessage);
+    expect(emailStatus.title).toBe(validationError);
     expect(emailStatus.textContent).toBe('🔴');
   });
 
   test('Should show password error if validation fails', () => {
-    const { sut, validationStub } = makeSut();
+    const validationError = faker.random.words();
+    const { sut } = makeSut({ validationError });
     const passwordInput = sut.getByTestId('password');
     fireEvent.input(passwordInput, {
       target: { value: faker.internet.password() },
     });
     const passwordStatus = sut.getByTestId('password-status');
-    expect(passwordStatus.title).toBe(validationStub.erroMessage);
+    expect(passwordStatus.title).toBe(validationError);
     expect(passwordStatus.textContent).toBe('🔴');
   });
 
   test('Should show valid email state if validation succeeds', () => {
-    const { sut, validationStub } = makeSut();
-    validationStub.erroMessage = null;
+    const { sut } = makeSut();
     const emailInput = sut.getByTestId('email');
     fireEvent.input(emailInput, {
       target: { value: faker.internet.email() },
@@ -74,8 +80,7 @@ describe('Login Component', () => {
   });
 
   test('Should show valid password state if validation succeeds', () => {
-    const { sut, validationStub } = makeSut();
-    validationStub.erroMessage = null;
+    const { sut } = makeSut();
     const passwordInput = sut.getByTestId('password');
     fireEvent.input(passwordInput, {
       target: { value: faker.internet.password() },
@@ -86,8 +91,7 @@ describe('Login Component', () => {
   });
 
   test('Should enable submit button if form is valid', () => {
-    const { sut, validationStub } = makeSut();
-    validationStub.erroMessage = null;
+    const { sut } = makeSut();
     const emailInput = sut.getByTestId('email');
     fireEvent.input(emailInput, {
       target: { value: faker.internet.email() },
