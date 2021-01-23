c4af570752b0fdd0b34a1d98f193ccc7acfab4f1
ClailsonJorge <clailsonjorge@gmail.com>
2021-01-23 11:54:04 -0300

    refactor: created factory method

--- src/validation/min-length/min-length-validation.spec.ts
+++ src/validation/min-length/min-length-validation.spec.ts
@@ -1,23 +1,18 @@
+import faker from 'faker'
 import { InvalidFieldError } from '../erros'
-import { FieldValidation } from '../protocols/field-validation'
+import { MinLengthValidation } from './min-length-validation'
 
-class MinLengthValidation implements FieldValidation {
-  constructor (public readonly field: string, private readonly minLength: number) {}
-
-  validate (value: string): Error {
-    return value.length >= this.minLength ? null : new InvalidFieldError()
-  }
-}
+const makeSut = (): MinLengthValidation => new MinLengthValidation(faker.database.column(), 5)
 
 describe('MinLengthValidation', () => {
   test('Should return error if value is invalid', () => {
-    const sut = new MinLengthValidation('field', 5)
+    const sut = makeSut()
     const error = sut.validate('123')
     expect(error).toEqual(new InvalidFieldError())
   })
 
   test('Should return falsy if value is valid', () => {
-    const sut = new MinLengthValidation('field', 5)
+    const sut = makeSut()
     const error = sut.validate('12345')
     expect(error).toBeFalsy()
   })
